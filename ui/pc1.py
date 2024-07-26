75de5dfbbe5c390fe39ec53641c09ed9de3d29fd
byeron <disce.uno@gmail.com>
2024-07-26 10:34:29 +0900

    fix: Consolidate common logic

--- ui/pc1.py
+++ ui/pc1.py
@@ -2,23 +2,47 @@ import typer
 
 from usecase.common import line_plot, run_common
 from domain.common_option import get_common_options
+from usecase.ela import run_ela
 
 pc1 = typer.Typer()
+mode = "pc1"
 
 
 @pc1.command()
-def run(
+def bin(
     ctx: typer.Context,
     path: str = typer.Argument(..., help="Path to the input data"),
     top_n: int = typer.Option(7, help="Top nth lowest p-value features"),
 ):
     # PCAの第一主成分の重みの上位を選択する
-    mode = "pc1"
     options = get_common_options(ctx)
-    indices, weights = run_common(path, top_n, mode, options)
+    _, indices, weights = run_common(path, top_n, mode, options)
 
     if options.viz:
         line_plot(indices, weights, mode, options.imgdir)
+
+
+@pc1.command()
+def ela(
+    ctx: typer.Context,
+    path: str = typer.Argument(
+        ...,
+        help="Path to the input data",
+    ),
+    top_n: int = typer.Option(
+        7,
+        help="Top n coefficient of variation features",
+    ),
+    energy_th: float = typer.Option(
+        None,
+        help="Threshold of energy",
+    ),
+):
+    options = get_common_options(ctx)
+    bins, _, _ = run_common(path, top_n, mode, options)
+
+    # ELAを計算する
+    run_ela(bins, energy_threshold=energy_th)
 
 
 if __name__ == "__main__":
--- ui/peak.py
+++ ui/peak.py
@@ -1,23 +1,46 @@
 import typer
 from usecase.common import line_plot, run_common
 from domain.common_option import get_common_options
+from usecase.ela import run_ela
 
 peak = typer.Typer()
+mode = "peak"
 
 
 @peak.command()
-def run(
+def bin(
     ctx: typer.Context,
     path: str = typer.Argument(..., help="Path to the input file"),
     top_n: int = typer.Option(7, help="Number of peaks"),
 ):
-
-    mode = "peak"
     options = get_common_options(ctx)
-    indices, origin = run_common(path, top_n, mode, options)
+    _, indices, origin = run_common(path, top_n, mode, options)
 
     if options.viz:
         line_plot(indices, origin, mode, options.imgdir)
+
+
+@peak.command()
+def ela(
+    ctx: typer.Context,
+    path: str = typer.Argument(
+        ...,
+        help="Path to the input data",
+    ),
+    top_n: int = typer.Option(
+        7,
+        help="Top n coefficient of variation features",
+    ),
+    energy_th: float = typer.Option(
+        None,
+        help="Threshold for energy",
+    ),
+):
+    options = get_common_options(ctx)
+    bins, _, _ = run_common(path, top_n, mode, options)
+
+    # ELAを計算する
+    run_ela(bins, energy_threshold=energy_th)
 
 
 if __name__ == "__main__":
